c9cd3f72234d83d5b10613eb77dc4045d79e7c6f
pougem <pougem@pc-b102-01.iut-valence.fr>
2013-01-30 17:49:00 +0100

    ajout images de cartes

--- gen/com/example/cartes/R.java
+++ gen/com/example/cartes/R.java
@@ -18,6 +18,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
         public static final int koala=0x7f020002;
         public static final int penguins=0x7f020003;
+        public static final int voiture=0x7f020004;
     }
     public static final class id {
         public static final int bt_instructions=0x7f080001;
